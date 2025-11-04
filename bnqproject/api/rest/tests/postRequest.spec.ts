673b5ea09c003296a3a03eb440a967618a57a862
Avani K(UST <280665@ust.com>
2025-11-04 15:01:27 +0530

    added zod

--- bnqproject/api/rest/tests/postRequest.spec.ts
+++ bnqproject/api/rest/tests/postRequest.spec.ts
@@ -1,8 +1,12 @@
 import { test, expect } from "../../fixtures/APIFixture";
+import { userSchema,User } from "../../schema/UserSchema";
 
 test("create user", async ({ restClient }) => {
-  const customData = { Name: "Tom", age: "24" };
-
+  // const customData = { Name: "Tom", age: "24" };
+  const user: User={};
+  user.name="Tom";
+  user.age=24;
+  const customData=userSchema.parse(user);
   const { response, payload } = await restClient.createUser(customData);
   expect(response.status()).toBe(201);
   const body = await response.json();
--- screwfixproject/api/rest/tests/postRequest.spec.ts
+++ screwfixproject/api/rest/tests/postRequest.spec.ts
@@ -1,11 +1,18 @@
 import { test, expect } from "../../fixtures/APIFixture";
+import { userSchema,User } from "../../schema/UserSchema";
 
 test("create user", async ({ restClient }) => {
-  const customData = { Name: "Tom", age: "24" };
+
+  const user: User={};
+  user.name="Tom";
+  user.age=24;
+  user.job="Developer";
+  const customData=userSchema.parse(user);
   const { response, payload } = await restClient.createUser(customData);
   expect(response.status()).toBe(201);
   const body = await response.json();
   console.log(body);
   expect(body.name).toBe(payload.name);
   expect(body.job).toBe(payload.job);
+  expect(body.age).toBe(payload.age);
 });
